fbdcad10bcd10c5d5937bb45c92e580053ef2bb3
Jaime <79403778+Jaimepas77@users.noreply.github.com>
2025-04-06 18:09:30 +0200

    Cache userID
    Si el username es el mismo que la última vez, entonces se usa el useID cacheado de la anterior vez. Además se elimina la función de webscrapping (no se necesita ya) y se asegura que la tabla de problemas está cargada antes de colorearla.

--- scripts/content.js
+++ scripts/content.js
@@ -96,6 +96,16 @@ window.onload = async function () {
 async function highlightTitles() {
     console.log("Ini of AeRForU: highlighting problems")
     if (username !== false) {
+        let finalTable = document.getElementById("problemsInfo").children[1].children[3];
+        try {
+            finalTable.children[0].children[1].innerText.trim(); // Intentar acceder a un elemento para verificar si la tabla está cargada
+        }
+        catch (error) {
+            console.log("Table not found yet, waiting...");
+            setTimeout(highlightTitles, 100);
+            return;
+        }
+
         const problemsInfo = document.getElementById("problemsInfo");
 
         if (problemsInfo === null) return;
@@ -111,12 +121,12 @@ async function highlightTitles() {
         const table = problemsInfo.children[1];
         // console.log(table);
 
-        const problemNodes = table.children[3]
+        const problemNodes = table.children[3];
         // console.log(problemNodes.children);
 
         for (const problem of problemNodes.children) {
             const title = problem.children[1].innerText.trim();
-            console.log("Title: " + title);
+            // console.log("Title: " + title);
             //if (wordsToAc.length > 0 && wordsToAc.includes(title)) {
             if (await isAC(problem, userID)) {
                 problem.style.backgroundColor = AcColor;
@@ -139,7 +149,7 @@ async function highlightTitles() {
 async function addError(problem, userID) {
     // Get the problem ID
     const problemId = problem.children[0].innerText.trim();
-    console.log("Problem ID: " + problemId);
+    // console.log("Problem ID: " + problemId);
 
     // Get the last submission
     let problem_submissions_url = "https://aceptaelreto.com/ws/user/${userID}/submissions/problem/${problemId}";
@@ -229,80 +239,54 @@ async function addError(problem, userID) {
 
 }
 
-//Function to get the words to highlight
-async function getInfo() {
-    const wordsAc = [];
-    const wordsWa = [];
-
-    //We need to webscrap the words to highlight
-    const baseSearchUrl = "https://aceptaelreto.com/bin/search.php?search_query=${username}&commit=searchUser&search_currentPage=%2Fuser%2Fprofile.php";
-    url = baseSearchUrl.replace("${username}", username);
-    //console.log("URL: " + url);
-    //We need to make a request to the url
-    const request = await fetch(url);
-
-    const rText = await request.text();
-    // console.log(rText);
-
-    //Get the user ID
-    const finalUrl = request.url;
-    // console.log("Final URL: " + finalUrl);
-    const userID = finalUrl.split("id=")[1];
-    // console.log("User ID: " + userID);
-
-    async function fillWords(AC) {
-        //Filter with the regex ">something - something</a> (accepts anything)
-        //width if ac=true, bug if ac=false
-        const regex1 = new RegExp(`${AC ? "width" : "bug"} text-muted[^c]+>[0-9]+ - [^<]+<\/a>`, "gi"); //Only AC
-        const regex2 = new RegExp(">[0-9]+ - [^<]+</a>", "gi"); //Only the title part
-        //const regex = new RegExp(">[a-zA-Z0-9]+ - [a-zA-Z0-9 ]+</a>", "gi");
-        const matches1 = rText.match(regex1);
-        if (matches1 === null) return;
-        //Matches is matches1 that matches regex2
-        const matches = matches1.join("").match(regex2);
-        //console.log("Hey: " + matches);
-
-        words = matches.map(match => {
-            //Remove the > and </a>
-            return match.substring(7, match.length - 4);
-        });
-
-        //wordsAc.push(...words);
-        (AC ? wordsAc : wordsWa).push(...words);
-        //Escape the regex characters
-        (AC ? wordsAc : wordsWa).forEach((word, index) => {
-            (AC ? wordsAc : wordsWa)[index] = word.replace(/\[\.\*\+\?\^\$\{\}\(\)\|\[\]\\]/g, '');
-        });
-    }
-    await fillWords(true);
-    await fillWords(false);
-
-    console.log("Words to AC: " + wordsAc.length);
-    console.log("Words to WA: " + wordsWa.length);
-
-    return { wordsAc, wordsWa, userID };
-}
-
 async function getUserID() {
-    const baseSearchUrl = "https://aceptaelreto.com/bin/search.php?search_query=${username}&commit=searchUser&search_currentPage=%2Fuser%2Fprofile.php";
-    url = baseSearchUrl.replace("${username}", username);
-    
-    //We need to make a request to the url
-    const request = await fetch(url, {
-        method: 'HEAD',
-        redirect: 'follow'
+    let prevUsername = await new Promise((resolve) => {
+        chrome.storage.local.get("username", function (data) {
+            resolve(data.username);
+        });
     });
 
-    //Get the user ID
-    const finalUrl = request.url;
-    // console.log("Final URL: " + finalUrl);
-    return finalUrl.split("id=")[1];
+    let userID;
+    if (prevUsername === username) {
+        userID = await new Promise((resolve) => {
+            chrome.storage.local.get("userID", function (data) {
+                resolve(data.userID);
+            });
+        });
+    }
+    else if (prevUsername !== undefined) {
+        console.log("New username: " + username);
+        const baseSearchUrl = "https://aceptaelreto.com/bin/search.php?search_query=${username}&commit=searchUser&search_currentPage=%2Fuser%2Fprofile.php";
+        url = baseSearchUrl.replace("${username}", username);
+        
+        //We need to make a request to the url
+        const request = await fetch(url, {
+            method: 'HEAD',
+            redirect: 'follow'
+        });
+
+        //Get the user ID
+        const finalUrl = request.url;
+        // console.log("Final URL: " + finalUrl);
+        userID = finalUrl.split("id=")[1];
+
+        // Strore new username and userID in the storage
+        chrome.storage.local.set({ username: username });
+        chrome.storage.local.set({ userID: userID });
+    }
+    else {
+        console.log("No username found");
+        userID = false;
+    }
+
+    console.log("User ID: " + userID);
+    return userID;
 }
 
 async function isAC(problem, userID) {
     // Get the problem ID
     const problemId = problem.children[0].innerText.trim();
-    console.log("Problem ID: " + problemId);
+    // console.log("Problem ID: " + problemId);
 
     // Search for an AC submission
     let problem_submissions_url = "https://aceptaelreto.com/ws/user/${userID}/submissions/problem/${problemId}";
@@ -335,7 +319,7 @@ async function isAC(problem, userID) {
 async function isTried(problem, userID) {
     // Get the problem ID
     const problemId = problem.children[0].innerText.trim();
-    console.log("Problem ID: " + problemId);
+    // console.log("Problem ID: " + problemId);
 
     // Search for an AC submission
     let problem_submissions_url = "https://aceptaelreto.com/ws/user/${userID}/submissions/problem/${problemId}";
