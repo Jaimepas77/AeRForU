38e0c6d5208bfe5ab76523e5fd869ab8f402c83b
Jaime <79403778+Jaimepas77@users.noreply.github.com>
2025-10-04 16:51:34 +0200

    Update getters.spec.js

--- scripts/getters.spec.js
+++ scripts/getters.spec.js
@@ -1,3 +1,8 @@
+/*
+To run the tests, use this command in the root folder of the project:
+npm test
+*/
+
 const { isAC, isTried, getUserID, getNick, getLastError, getProblemCategories, isProblemsCategory, getCategoryData, getProblemLevel } = require('./getters');
 const levels_dict = require('../data/levels.js');
 
